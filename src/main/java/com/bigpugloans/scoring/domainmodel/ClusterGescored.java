52667e6a05b78dbb65b69d2069eaa24d869aae61
Michael Plöd <michael.ploed@gmail.com>
2024-10-01 07:37:14 +0200

    Cluster Status eingeführt

--- src/main/java/com/bigpugloans/scoring/domainmodel/ClusterGescored.java
+++ src/main/java/com/bigpugloans/scoring/domainmodel/ClusterGescored.java
@@ -1,12 +1,18 @@
 package com.bigpugloans.scoring.domainmodel;
 
-public record ClusterGescored(Punkte punkte, KoKriterien koKriterien) {
+public record ClusterGescored(Punkte punkte, KoKriterien koKriterien, ClusterStatus clusterStatus) {
     public ClusterGescored(Punkte punkte) {
-        this(punkte, new KoKriterien(0));
+        this(punkte, new KoKriterien(0), ClusterStatus.GESCORED);
     }
 
     public ClusterGescored(Punkte punkte, int koKriterien) {
-        this(punkte, new KoKriterien(koKriterien));
+        this(punkte, new KoKriterien(koKriterien), ClusterStatus.GESCORED);
+    }
+    public ClusterGescored(Punkte punkte, KoKriterien koKriterien) {
+        this(punkte, koKriterien, ClusterStatus.GESCORED);
+    }
+    public ClusterGescored() {
+        this(new Punkte(0), new KoKriterien(0), ClusterStatus.NICHT_GESCORED);
     }
 
 
---
+++ src/main/java/com/bigpugloans/scoring/domainmodel/ClusterStatus.java
@@ -0,0 +1,5 @@
+package com.bigpugloans.scoring.domainmodel;
+
+public enum ClusterStatus {
+    GESCORED, NICHT_GESCORED
+}

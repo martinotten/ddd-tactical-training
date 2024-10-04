cab5e1a004b296a54924b17186dacdd5ec502170
Michael Plöd <michael.ploed@gmail.com>
2024-10-04 07:34:08 +0200

    Antragsnummer samt Test hinzugefügt

---
+++ src/main/java/com/bigpugloans/scoring/domainmodel/Antragsnummer.java
@@ -0,0 +1,38 @@
+package com.bigpugloans.scoring.domainmodel;
+
+import java.util.Objects;
+
+public class Antragsnummer {
+    private String antragsnummer;
+
+    public Antragsnummer(String antragsnummer) {
+        if(antragsnummer == null) {
+            throw new IllegalArgumentException("Antragsnummer darf nicht null sein.");
+        }
+        this.antragsnummer = antragsnummer;
+    }
+
+    public String nummer() {
+        return antragsnummer;
+    }
+
+    @Override
+    public String toString() {
+        return "Antragsnummer{" +
+                "antragsnummer='" + antragsnummer + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Antragsnummer that = (Antragsnummer) o;
+        return Objects.equals(antragsnummer, that.antragsnummer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(antragsnummer);
+    }
+}
---
+++ src/test/java/com/bigpugloans/scoring/domainmodel/AntragsnummerTest.java
@@ -0,0 +1,21 @@
+package com.bigpugloans.scoring.domainmodel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AntragsnummerTest {
+    @Test
+    void testAntragsnummer() {
+        Antragsnummer antragsnummer = new Antragsnummer("123456");
+        assertEquals("123456", antragsnummer.nummer());
+    }
+
+    @Test
+    void testAntragsnummerNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Antragsnummer(null);
+        });
+    }
+}

7ce182b1079a8ff862fd02a6a2c66414ad45a1b9
Michael Plöd <michael.ploed@gmail.com>
2024-10-05 07:04:39 +0200

    Antragsnummer in MonatlicheFinanzsituationClusterTest als ID berücksichtigt

--- src/test/java/com/bigpugloans/scoring/domainmodel/monatlicheFinanzsituationCluster/MonatlicheFinanzsituationClusterTest.java
+++ src/test/java/com/bigpugloans/scoring/domainmodel/monatlicheFinanzsituationCluster/MonatlicheFinanzsituationClusterTest.java
@@ -1,17 +1,40 @@
 package com.bigpugloans.scoring.domainmodel.monatlicheFinanzsituationCluster;
 
+import com.bigpugloans.scoring.domainmodel.Antragsnummer;
 import com.bigpugloans.scoring.domainmodel.ClusterGescored;
 import com.bigpugloans.scoring.domainmodel.Punkte;
 import com.bigpugloans.scoring.domainmodel.Waehrungsbetrag;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MonatlicheFinanzsituationClusterTest {
     @Test
+    void monatlicheFinanzsituationClusterOhneAntragsnummerWirftException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new MonatlicheFinanzsituationCluster(null);
+        });
+    }
+
+    @Test
+    void monatlicheFinanzsituationClusterMitGleicherAntragsnummerSindGleich() {
+        Antragsnummer antragsnummer = new Antragsnummer("123");
+
+        MonatlicheFinanzsituationCluster monatlicheFinanzsituationCluster1 = new MonatlicheFinanzsituationCluster(antragsnummer);
+        monatlicheFinanzsituationCluster1.monatlicheEinnahmenHinzufuegen(new Waehrungsbetrag(3000));
+        monatlicheFinanzsituationCluster1.monatlicheAusgabenHinzufuegen(new Waehrungsbetrag(1000));
+        monatlicheFinanzsituationCluster1.monatlicheDarlehensbelastungenHinzufuegen(new Waehrungsbetrag(10));
+
+        MonatlicheFinanzsituationCluster monatlicheFinanzsituationCluster2 = new MonatlicheFinanzsituationCluster(antragsnummer);
+        monatlicheFinanzsituationCluster2.monatlicheEinnahmenHinzufuegen(new Waehrungsbetrag(2000));
+        monatlicheFinanzsituationCluster2.monatlicheAusgabenHinzufuegen(new Waehrungsbetrag(1500));
+        monatlicheFinanzsituationCluster2.monatlicheDarlehensbelastungenHinzufuegen(new Waehrungsbetrag(2000));
+
+        assertEquals(monatlicheFinanzsituationCluster1, monatlicheFinanzsituationCluster2, "Beide MonatlicheFinanzsituationCluster sollten gleich sein.");
+    }
+    @Test
     void monatlicheDarlehensbelastungGroesserEinnahmenMinusAusgabenIstKoKriterium() {
-        MonatlicheFinanzsituationCluster monatlicheFinanzsituationCluster = new MonatlicheFinanzsituationCluster();
+        MonatlicheFinanzsituationCluster monatlicheFinanzsituationCluster = new MonatlicheFinanzsituationCluster(new Antragsnummer("123"));
         monatlicheFinanzsituationCluster.monatlicheEinnahmenHinzufuegen(new Waehrungsbetrag(3000));
         monatlicheFinanzsituationCluster.monatlicheAusgabenHinzufuegen(new Waehrungsbetrag(1000));
         monatlicheFinanzsituationCluster.monatlicheDarlehensbelastungenHinzufuegen(new Waehrungsbetrag(2500));
@@ -20,7 +43,7 @@ public class MonatlicheFinanzsituationClusterTest {
     }
     @Test
     void monatlicherHaushaltsueberschussOhneTilgungenUeber1500Gibt15Punkte() {
-        MonatlicheFinanzsituationCluster monatlicheFinanzsituationCluster = new MonatlicheFinanzsituationCluster();
+        MonatlicheFinanzsituationCluster monatlicheFinanzsituationCluster = new MonatlicheFinanzsituationCluster(new Antragsnummer("123"));
         monatlicheFinanzsituationCluster.monatlicheEinnahmenHinzufuegen(new Waehrungsbetrag(2600));
         monatlicheFinanzsituationCluster.monatlicheAusgabenHinzufuegen(new Waehrungsbetrag(1000));
 

a8da74f2b76ee3a6d8ca128b4412c4bafa866088
Michael Plöd <michael.ploed@gmail.com>
2024-10-09 10:55:58 +0200

    Application Service dependencies

---
+++ src/main/java/com/bigpugloans/scoring/application/service/PreScoringStartApplicationService.java
@@ -0,0 +1,22 @@
+package com.bigpugloans.scoring.application.service;
+
+import com.bigpugloans.scoring.application.model.Antrag;
+import com.bigpugloans.scoring.application.ports.driven.*;
+import com.bigpugloans.scoring.application.ports.driving.PreScoringStart;
+import com.bigpugloans.scoring.domainmodel.scoringErgebnis.ScoringErgebnis;
+
+public class PreScoringStartApplicationService implements PreScoringStart {
+    private ScoringErgebnisRepository scoringErgebnisRepository;
+    private AntragstellerClusterRepository antragstellerClusterRepository;
+    private MonatlicheFinanzsituationClusterRepository;
+    private AuskunfteiErgebnisClusterRepository auskunfteiErgebnisClusterRepository;
+    private ImmobilienFinanzierungClusterRepository immobilienFinanzierungClusterRepository;
+
+    private KonditionsAbfrage konditionsAbfrage;
+    private LeseKontoSaldo leseKontoSaldo;
+
+    @Override
+    public void startePreScoring(Antrag antrag) {
+
+    }
+}

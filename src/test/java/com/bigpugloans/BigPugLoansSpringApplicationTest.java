600c138f33ecc64e86f368f9ce9ba2ad63ee8c12
Michael Plöd <michael.ploed@gmail.com>
2024-10-16 07:55:05 +0200

    Spring Boot, Spring Data JDCB und H2 hinzugefügt

---
+++ src/test/java/com/bigpugloans/BigPugLoansSpringApplicationTest.java
@@ -0,0 +1,13 @@
+package com.bigpugloans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigPugLoansSpringApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

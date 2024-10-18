8ed4ac3849bfceca78ea68421467a8faa0837b78
Michael Plöd <michael.ploed@gmail.com>
2024-10-18 07:57:29 +0200

    Spring application hinzugefügt

---
+++ src/main/java/com/bigpugloans/BigPugLoansSpringApplication.java
@@ -0,0 +1,11 @@
+package com.bigpugloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigPugLoansSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BigPugLoansSpringApplication.class, args);
+    }
+}
